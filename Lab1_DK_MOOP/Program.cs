50549a9bbb9835b29ed75c40ce38074619d5bf9c
Ilya <ilya.deyev@gmail.com>
2023-03-28 17:09:07 +0300

    Intial commit

---
+++ Lab1_DK_MOOP/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lab1_DK_MOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Я заебался!!!");
+        }
+    }
+}
